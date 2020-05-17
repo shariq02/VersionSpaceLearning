c93a7de2edb789786f5c47b66f14b9787126e433
harsh <harsh@LAPTOP-IM21917R>
2020-05-18 00:54:42 +0200

    JSON file is readable using MAVEN dependencies

---
+++ spabDSS_IV/src/main/java/com/DSS_IV/app/App.java
@@ -0,0 +1,53 @@
+package com.DSS_IV.app;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+ 
+public class App 
+{
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) 
+    {
+        //JSON parser object to parse read file
+        JSONParser jsonParser = new JSONParser();
+         
+        try (FileReader reader = new FileReader("sample_output.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+ 
+            JSONArray employeeList = (JSONArray) obj;
+             
+            System.out.println("firstName \t lastName \t website");
+            //Iterate over employee array
+            employeeList.forEach( emp -> parseEmployeeObject( (JSONObject) emp ) );
+ 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    private static void parseEmployeeObject(JSONObject employee) 
+    {
+        //Get employee object within list
+        JSONObject employeeObject = (JSONObject) employee.get("employee");
+         
+        //Get employee first name
+        String firstName = (String) employeeObject.get("firstName");    
+        //Get employee last name
+        String lastName = (String) employeeObject.get("lastName");           
+        //Get employee website name
+        String website = (String) employeeObject.get("website");
+        System.out.println(firstName+"\t"+lastName+"\t"+website);
+    }
+}
